0321d5cf2bc771ed1fdb645fd05bd376620998cf
mandeer <mandeer163@163.com>
2018-02-06 23:00:52 +0800

    train LeNet in mnist

--- main_LeNet.py
+++ main_LeNet.py
@@ -14,21 +14,25 @@ class Solver(object):
         self.trainLoader = trainLoader
         self.testLoader = testLoader
         self.LeNet = getattr(models, 'LeNet')()
+        if config.modelName != '':
+            print('use pretrained model: ', config.modelName)
+            self.LeNet.load(config.modelName)
         self.optimizer = torch.optim.SGD(self.LeNet.parameters(), lr=config.lr, weight_decay=config.weightDecay)
         self.n_epochs = config.n_epochs
         self.logStep = config.logStep
+        self.outPath = config.outPath
 
-    def test(self):
-        model = self.LeNet
+    def val(self):
+        LeNet = self.LeNet
         testLoader = self.testLoader
-        model.eval()  # 验证模式
+        LeNet.eval()  # 验证模式
         class_correct = list(0. for i in range(10))
         class_total = list(0. for i in range(10))
         accuracy = list(0. for i in range(10 + 1))
         for ii, (datas, labels) in enumerate(testLoader):
             val_inputs = Variable(datas, volatile=True)
             # print(labels)
-            outputs = model(val_inputs)
+            outputs = LeNet(val_inputs)
             _, predicted = torch.max(outputs.data, 1)
             c = (predicted == labels).squeeze()
             for jj in range(labels.size()[0]):
@@ -47,10 +51,13 @@ class Solver(object):
                 accuracy[ii] = class_correct[ii] / class_total[ii]
         accuracy[10] = correct / total
 
-        model.train()  # 训练模式
+        LeNet.train()  # 训练模式
         return accuracy
 
     def train(self):
+        val_accuracy = self.val()
+        print('begin with accuracy: ', val_accuracy[10])
+
         LeNet = self.LeNet
         criterion = torch.nn.CrossEntropyLoss()
         for epoch in range(self.n_epochs):
@@ -67,10 +74,21 @@ class Solver(object):
                 if (ii + 1) % self.logStep == 0:
                     print('epoch: ', epoch, 'train_num: ', ii + 1, loss.data.numpy())
 
-            val_accuracy = self.test()
+            val_accuracy = self.val()
             # for jj in range(10):
             #     print('accuracy_', jj, ': ', val_accuracy[jj])
             print('accuracy total: ', val_accuracy[10])
+
+        LeNet.save(root=self.outPath, name='LeNet_mnist.pth')
+        return
+
+    def test(self):
+        accuracy = self.val()
+
+        for jj in range(10):
+            print('accuracy_', jj, ': ', accuracy[jj])
+        print('accuracy total: ', accuracy[10])
+        return
 
 
 def main(config):
@@ -90,27 +108,29 @@ def main(config):
     solver = Solver(config, trainLoader, testLoader)
     print(solver.LeNet)
 
-    solver.train()
-
+    if config.mode == 'train':
+        solver.train()
+    elif config.mode == 'test':
+        solver.test()
+    else:
+        print('error mode!')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    # model hyper-parameters
     parser.add_argument('--imageSize', type=int, default=32)
-
-    # training hyper-parameters
     parser.add_argument('--n_epochs', type=int, default=10)
     parser.add_argument('--batchSize', type=int, default=64)
     parser.add_argument('--n_workers', type=int, default=4)
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--weightDecay', type=float, default=1e-4, help='')
-
-    # other
     parser.add_argument('--outPath', type=str, default='./output')
     parser.add_argument('--mnistPath', type=str, default='./data/mnist')
     parser.add_argument('--logStep', type=int, default=100)
     parser.add_argument('--cuda', type=str2bool, default=True, help='enables cuda')
+
+    parser.add_argument('--mode', type=str, default='test', help='train, test')
+    parser.add_argument('--modelName', type=str, default='./pretrained_models/LeNet_mnist.pth', help='model for test or retrain')
 
     config = parser.parse_args()
     if config.cuda and not torch.cuda.is_available():
---
+++ pretrained_models/readme.txt
@@ -0,0 +1,4 @@
+pretrained_models
+
+1, LeNet_mnist.pth 10次迭代，在mnist数据集上的识别结果为0.983
+2，
