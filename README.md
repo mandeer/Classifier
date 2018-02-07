08d59876d29bbfe1a1484904bf6c995f9edb1d43
mandeer <mandeer163@163.com>
2018-02-07 22:48:18 +0800

    add AlexNet

--- README.md
+++ README.md
@@ -20,10 +20,29 @@
 
 简化了网络参数并使网络具有一定程度的位移、尺度、形状不变性
 
-### 本工程实现的LeNet与LeNet-5略有区别
+### 本工程实现的LeNet与原始的LeNet-5略有区别
 1. 使用了ReLU而不是sigmoid函数
 2. S2中的每个特征图连接到了每个C3的特征图
 
 ### 训练结果
 1. 在mnist进行了10次迭代，最终在测试集上的识别结果为 0.983
 2. 在cifar10进行了40次迭代，最终测试集上的识别结果为 0.636
+
+
+## 2. AlexNet [paper](https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf)
+AlexNet在2012年的ImageNet图像分类竞赛中，top-5错误率比上一年的冠军下降了十个百分点，
+且远远超过当年的第二名。将沉寂多年的神经网络重新引入大众的视野。
+![AlexNet](./imgs/AlexNet.png)
+### 主要贡献
+1. 非线性激活函数: ReLU
+2. 防止过拟合的方法: Dropout, Data augmentation
+3. 大数据训练: imageNet
+4. 高性能计算平台: GPU
+5. 重叠Pooling: kernel_size=3, stride=2
+6. 局部相应归一化(LRN, Local Response Normalization), VGG说这个没什么用
+
+### 本工程实现的AlexNet与原始的AlexNet略有区别
+1. 没有使用LRN
+2. 没有使用group
+
+## 3. VGG [paper](https://arxiv.org/abs/1409.1556)
--- models/AlexNet.py
+++ models/AlexNet.py
@@ -11,11 +11,59 @@ model_urls = {
 }
 
 
-class AlexNet_CIFAR(BasicModule):
+class AlexNet(BasicModule):
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.model_name = 'alexnet_cifar'
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
 
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+
+
+def alexnet(pretrained=False, **kwargs):
+    r"""AlexNet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = AlexNet(**kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['alexnet']))
+    return model
+
+
+class AlexNet_CIFAR(BasicModule):
     def __init__(self, num_classes=10):
         super(AlexNet_CIFAR, self).__init__()
-        self.model_name = 'alexnet'
+        self.model_name = 'alexnet_cifar'
         self.features = nn.Sequential(
             nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
