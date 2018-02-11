d6a5422d7e68ed1dcf55085a79f90234c5ab74c6
mandeer <mandeer163@163.com>
2018-02-11 21:17:09 +0800

    update NIN

--- models/NIN.py
+++ models/NIN.py
@@ -4,50 +4,51 @@ import math
 import torch.nn as nn
 from .BasicModule import BasicModule
 
+
+def makeMlpConv(in_channel, hid_channel, out_channel, kernel_size=3, stride=1, padding=1):
+    assert isinstance(hid_channel, int) or len(hid_channel) == 2
+    if isinstance(hid_channel, int):
+        hid1 = hid_channel
+        hid2 = hid_channel
+    else:
+        hid1 = hid_channel[0]
+        hid2 = hid_channel[1]
+
+    return nn.Sequential(
+        nn.Conv2d(in_channel, hid1, kernel_size=kernel_size, stride=stride, padding=padding),
+        nn.BatchNorm2d(hid1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(hid1, hid2, kernel_size=1, stride=1, padding=0),
+        nn.BatchNorm2d(hid2),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(hid2, out_channel, kernel_size=1, stride=1, padding=0),
+        nn.BatchNorm2d(out_channel),
+        nn.ReLU(inplace=True),
+    )
+
+
 class NIN(BasicModule):
     def __init__(self, n_class, init_weights=True):
         super(NIN, self).__init__()
         self.model_name = 'nin'
         self.n_class = n_class
-        self.classifer = nn.Sequential(
-            nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(192),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(192, 160, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(160),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(160,  96, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(96),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-
-            nn.Conv2d(96, 192, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(192),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(192),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(192),
-            nn.ReLU(inplace=True),
-            nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
-
-            nn.Conv2d(192, 192, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(192),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(192),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(192, self.n_class, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(inplace=True),
-            nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
-        )
+        self.MlpConv1 = makeMlpConv(3, (192, 160), 96, kernel_size=5, stride=1, padding=2)
+        self.MlpConv2 = makeMlpConv(96, 192, 192, kernel_size=5, stride=1, padding=2)
+        self.MlpConv3 = makeMlpConv(192, 192, self.n_class, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.pool2 = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
+        self.pool3 = nn.AvgPool2d(kernel_size=8, stride=1, padding=0)
 
         if init_weights:
             self._initialize_weights()
 
     def forward(self, x):
-        x = self.classifer(x)
+        x = self.MlpConv1(x)
+        x = self.pool1(x)
+        x = self.MlpConv2(x)
+        x = self.pool2(x)
+        x = self.MlpConv3(x)
+        x = self.pool3(x)
         x = x.view(x.size(0), self.n_class)
         return x
 
