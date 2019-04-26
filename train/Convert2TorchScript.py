b3e48bf023a0272725601d149958a18480be0380
mandeer <mandeer163@163.com>
2019-04-26 14:51:46 +0800

    clean code

--- train/Convert2TorchScript.py
+++ train/Convert2TorchScript.py
@@ -12,9 +12,12 @@ def main(config):
     if not os.path.exists(config.out_path):
         os.makedirs(config.out_path)
 
-    example = torch.rand(config.batch_size, config.input_c, config.input_h, config.input_w)
-    model = getattr(models, config.model)(num_classes=config.n_classes)
+    example = torch.ones(config.batch_size, config.input_c, config.input_h, config.input_w)
+    model = getattr(models, config.model)(depth=18, num_classes=config.n_classes)
+    model.load(config.torch_model)
     model.eval()
+    # output = model(example)
+    # print(output)
     traced_script_module = torch.jit.trace(model, example)
     traced_script_module.save(config.script_model)
 
@@ -24,13 +27,13 @@ if __name__ == '__main__':
 
     parser.add_argument('--batch-size',     type=int,       default=1)
     parser.add_argument('--input-c',        type=int,       default=3)
-    parser.add_argument('--input-h',        type=int,       default=32)
-    parser.add_argument('--input-w',        type=int,       default=32)
+    parser.add_argument('--input-h',        type=int,       default=224)
+    parser.add_argument('--input-w',        type=int,       default=224)
     parser.add_argument('--out-path',       type=str,       default='./output')
-    parser.add_argument('--model',          type=str,       default='MobileNetV2')
-    parser.add_argument('--n-classes',      type=int,       default=10)
-    parser.add_argument('--torch-model',    type=str,       default='./output/MobileNetV2_cifar10_50.pth')
-    parser.add_argument('--script-model',   type=str,       default='./output/MobileNetV2_cifar10_50.pt')
+    parser.add_argument('--model',          type=str,       default='ResNet')
+    parser.add_argument('--n-classes',      type=int,       default=1000)
+    parser.add_argument('--torch-model',    type=str,       default='./output/resnet18-5c106cde.pth')
+    parser.add_argument('--script-model',   type=str,       default='./output/resnet18.pt')
 
     config = parser.parse_args()
     args = vars(config)
